5e7b1cd7667d594412cdd449a7f67942e2ad7ba5
Raad <kronost037@gmail.com>
2026-05-20 21:27:48 +0600

    Abstract logic to Functions and fix bullet velocity according to direction

--- ATTRIBUTION.md
+++ ATTRIBUTION.md
@@ -1,1 +1,2 @@
+SKY PNG : [itch.io](https://itch.io/queue/c/302894/2d-backgrounds-for-games?game_id=1801829&password=)
 UFO PNG: [vecteezy.com](https://www.vecteezy.com/png/26773803-ufo-png-with-ai-generated)
--- game.c
+++ game.c
@@ -1,40 +1,72 @@
+#include <math.h>
 #include <stdlib.h>
 #include <raylib.h>
 #include <raymath.h>
 
-#define SCREEN_WIDTH 800
-#define SCREEN_HEIGHT 600
-#define UFO_SCALE 0.03f
+#define SCREEN_WIDTH 1500
+#define SCREEN_HEIGHT 900
+#define PLAYER_SCALE 0.03f
 #define MAX_BULLETS 10
 
-typedef struct S_ufo {
-    Vector2 ufo_pos;
-    Vector2 ufo_speed;
-    Vector2 ufo_dir;
-    Texture2D ufo_texture;
-} Ufo;
-
 typedef struct S_projectile {
+    bool active;
     Vector2 projectile_pos_start;
+    Vector2 projectile_size;
     Vector2 projectile_speed;
+    Vector2 projectile_dir;
+    Color projectile_color;
 } Projectile;
 
-void initializePlayer(Ufo *player) {
-    player->ufo_pos = (Vector2){0, 0};
-    player->ufo_speed = (Vector2){400, 400};
-    player->ufo_dir = (Vector2){0, 0};
-    player->ufo_texture = LoadTexture("ufo.png");
+typedef struct S_entity {
+    Texture2D entity_texture;
+    Vector2 entity_pos;
+    Vector2 entity_speed;
+    Vector2 entity_dir;
+    Projectile entity_bullets[MAX_BULLETS];
+} Entity;
+
+
+void initializePlayer(Entity *player) {
+    player->entity_texture = LoadTexture("Assets/ufo.png");
+    player->entity_pos = (Vector2){0};
+    player->entity_speed = (Vector2){600, 600};
+    player->entity_dir = (Vector2){0};
 }
 
-void makeBullet(Ufo *player, Projectile *bullets) {
-    Projectile bullet;
-    bullet.projectile_pos_start.x = player->ufo_pos.x + player->ufo_texture.width /2.0 * UFO_SCALE;
-    bullet.projectile_pos_start.y = player->ufo_pos.y;
-    
-    bullet.projectile_speed = (Vector2){0, 700};
+typedef struct S_game {
+    Texture2D sky_texture;
+    Entity player;
+} Game;
 
+void initializeGame(Game *game) {
+    game->player = (Entity){0};
+    initializePlayer(&game->player);
+    game->sky_texture = LoadTexture("Assets/sky.png");
+}
+
+void makeBullet(Entity *player, Projectile *bullets) {
+    Projectile bullet = {0};
+    
+    float halfPlayerWidth = (player->entity_texture.width * PLAYER_SCALE) / 2.0f;
+    float halfPlayerHeight = (player->entity_texture.height * PLAYER_SCALE) / 2.0f;
+    bullet.projectile_pos_start = (Vector2){ player->entity_pos.x + halfPlayerWidth,
+                                             player->entity_pos.y + halfPlayerHeight };
+    
+    bullet.projectile_dir = player->entity_dir;
+    if(bullet.projectile_dir.x == 0 && bullet.projectile_dir.y == 0) {
+        bullet.projectile_dir.y = -1;
+    } else {
+        bullet.projectile_dir = Vector2Normalize(bullet.projectile_dir);
+    }
+
+    bullet.projectile_size = (Vector2){ 4.0f, 20.0f }; 
+    
+    bullet.projectile_speed = (Vector2){ player->entity_speed.x + 800.0f, player->entity_speed.y + 800.0f }; 
+    bullet.projectile_color = RED;
+    bullet.active = true;
+    
     for(int i = 0; i < MAX_BULLETS; i++) {
-        if(bullets[i].projectile_pos_start.y == 0) {
+        if(!bullets[i].active) {
             bullets[i] = bullet;
             break;
         }
@@ -43,90 +75,108 @@ void makeBullet(Ufo *player, Projectile *bullets) {
 
 void fireBullet(Projectile *bullets) {
     for(int i = 0; i < MAX_BULLETS; i++) {
-        if(bullets[i].projectile_pos_start.y > 0) {
-            Vector2 start = bullets[i].projectile_pos_start;
-            Vector2 end = bullets[i].projectile_pos_start;
-            end.y -= 10;
-            DrawLineV(start, end, RED);
+        if(bullets[i].active) {
+            Vector2 bullet_end = {
+                bullets[i].projectile_pos_start.x - (bullets[i].projectile_dir.x * bullets[i].projectile_size.y),
+                bullets[i].projectile_pos_start.y - (bullets[i].projectile_dir.y * bullets[i].projectile_size.y)
+            };
+            
+            DrawLineEx(bullets[i].projectile_pos_start, bullet_end, bullets[i].projectile_size.x, bullets[i].projectile_color);
         }
     }
+}
+
+Vector2 getPlayerDirection() {
+    Vector2 entity_dir = {0};
+    
+    if(IsKeyDown(KEY_D)) {
+        entity_dir.x += 1;    
+    }
+    if(IsKeyDown(KEY_A)) {
+        entity_dir.x -= 1;
+    }
+    if(IsKeyDown(KEY_S)) {
+        entity_dir.y += 1;
+    }
+    if(IsKeyDown(KEY_W)) {
+        entity_dir.y -= 1;
+    }
+
+    return Vector2Normalize(entity_dir);
+}
+
+void handleCollision(Game *game) {
+    Entity *player = &game->player;
+    
+    float entity_scaledWidth = player->entity_texture.width * PLAYER_SCALE;
+    float entity_scaledHeight = player->entity_texture.height * PLAYER_SCALE;
+    
+    if (player->entity_pos.x < 0)
+        player->entity_pos.x = 0;
+    if (player->entity_pos.x > SCREEN_WIDTH - entity_scaledWidth)
+        player->entity_pos.x = SCREEN_WIDTH - entity_scaledWidth;
+    if (player->entity_pos.y < 0)
+        player->entity_pos.y = 0;
+    if (player->entity_pos.y > SCREEN_HEIGHT - entity_scaledHeight)
+        player->entity_pos.y = SCREEN_HEIGHT - entity_scaledHeight;
+    
+    for (int i = 0; i < MAX_BULLETS; i++) {
+        if (player->entity_bullets[i].projectile_pos_start.y < 0 ||
+            player->entity_bullets[i].projectile_pos_start.y > SCREEN_HEIGHT ||
+            player->entity_bullets[i].projectile_pos_start.x < 0 ||
+            player->entity_bullets[i].projectile_pos_start.x > SCREEN_WIDTH) {
+            
+            player->entity_bullets[i].active = false;
+        }
+    }
+}
+
+void runPhysics(Game *game) {
+    float delta_time = GetFrameTime();
+
+    Entity *player = &game->player;
+    
+    player->entity_dir = getPlayerDirection();
+    
+    player->entity_pos.x += player->entity_dir.x * player->entity_speed.x * delta_time;
+    player->entity_pos.y += player->entity_dir.y * player->entity_speed.y * delta_time;
+
+    if(IsKeyPressed(KEY_SPACE)) {
+        makeBullet(player, player->entity_bullets);
+    }
+    
+    for(int i = 0; i < MAX_BULLETS; i++) {
+        Projectile *bullet = &player->entity_bullets[i];
+        if(bullet->active) {
+            bullet->projectile_pos_start.x += bullet->projectile_dir.x * bullet->projectile_speed.x * delta_time;
+            bullet->projectile_pos_start.y += bullet->projectile_dir.y * bullet->projectile_speed.y * delta_time;
+        }
+    }
+
+    handleCollision(game);
 }
 
 int main() {
 	InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Space Shooter");
 
-    Ufo player = {0};
-    initializePlayer(&player);
+    Game game = {0};
+    initializeGame(&game);
 
-    Projectile bullets[MAX_BULLETS] = {0};
-    int bullets_count = 0;
-    
     while(!WindowShouldClose()) {
-        float delta_time = GetFrameTime();
-
-        // Movements
-        player.ufo_dir = (Vector2){0, 0};
         
-        if(IsKeyDown(KEY_D)) {
-            player.ufo_dir.x += 1;    
-        }
-        if(IsKeyDown(KEY_A)) {
-            player.ufo_dir.x -= 1;
-        }
-        if(IsKeyDown(KEY_S)) {
-            player.ufo_dir.y += 1;
-        }
-        if(IsKeyDown(KEY_W)) {
-            player.ufo_dir.y -= 1;
-        }
+        runPhysics(&game);
 
-        // Update Player Position
-        player.ufo_dir = Vector2Normalize(player.ufo_dir);
-        player. ufo_pos.x += player.ufo_dir.x * player.ufo_speed.x * delta_time;
-        player.ufo_pos.y += player.ufo_dir.y * player.ufo_speed.y * delta_time;
-
-        // Bullets
-        if(IsKeyPressed(KEY_SPACE) && bullets_count <= MAX_BULLETS) {
-            makeBullet(&player, bullets);
-            bullets_count++;
-        }
-
-        // Update Bullets Position
-        for(int i = 0; i < MAX_BULLETS; i++) {
-            bullets[i].projectile_pos_start.y -= bullets[i].projectile_speed.y * delta_time;
-        }
-        
-        // Collision
-        float ufo_scaledWidth = player.ufo_texture.width * UFO_SCALE;
-        float ufo_scaledHeight = player.ufo_texture.height * UFO_SCALE;
-        
-        if (player.ufo_pos.x < 0)
-            player.ufo_pos.x = 0;
-        if (player.ufo_pos.x > SCREEN_WIDTH - ufo_scaledWidth)
-            player.ufo_pos.x = SCREEN_WIDTH - ufo_scaledWidth;
-        if (player.ufo_pos.y < 0)
-            player.ufo_pos.y = 0;
-        if (player.ufo_pos.y > SCREEN_HEIGHT - ufo_scaledHeight)
-            player.ufo_pos.y = SCREEN_HEIGHT - ufo_scaledHeight;
-
-        for(int i = 0; i < MAX_BULLETS; i++) {
-            if(bullets[i].projectile_pos_start.y < 0) {
-                bullets[i].projectile_pos_start.y = 0;
-                bullets_count--;
-            } 
-        }
-        
-        // Display
         BeginDrawing();
         {
             ClearBackground(BLACK);
-            DrawTextureEx(player.ufo_texture, player.ufo_pos, 0.0f, UFO_SCALE, WHITE);
-            fireBullet(bullets);
+            DrawTextureEx(game.sky_texture, (Vector2){0, 0}, 0, 1.0f * SCREEN_WIDTH / game.sky_texture.height, WHITE);
+            DrawTextureEx(game.player.entity_texture, game.player.entity_pos, 0.0f, PLAYER_SCALE, WHITE);
+            fireBullet(game.player.entity_bullets);
         }
         EndDrawing();
     }
-    
-    UnloadTexture(player.ufo_texture);
+
+    UnloadTexture(game.player.entity_texture);
     CloseWindow();
 	return 0;
 }
